e577fc9c0b69a3a64a10b2e38e914658fefa2733
Mogbymo <mogbymo@gmail.com>
2018-11-09 20:23:17 +0300

    Fix style

--- vkapi/api.py
+++ vkapi/api.py
@@ -1,9 +1,11 @@
 import vk
 import time
 
+
 def get_api(access_token):
     session = vk.Session(access_token=access_token)
     api = vk.API(session, v='5.87')
+
 
 def group_name_from_url(url):
     if url.endswith('/'):
@@ -34,31 +36,32 @@ def get_group(api, url):
         'goods': list(iterate_call(api.market.get, 200, owner_id=-group_id))
     }
 
+
 def load_posts(api, community_id, count, user_token, version, offset = 0, verbose=True):
     current_offset = offset
     posts = []
     while current_offset < offset + count:
-            try:
-                if verbose:
-                    print('current_offset={}'.format(current_offset))
-                wall = api.wall.get(
-                    owner_id=-community_id,
-                    offset=current_offset,
-                    count=100,
-                    filter='owner',
-                    access_token=user_token,
-                    version=version
-                )
+        try:
+            if verbose:
+                print('current_offset={}'.format(current_offset))
+            wall = api.wall.get(
+                owner_id=-community_id,
+                offset=current_offset,
+                count=100,
+                filter='owner',
+                access_token=user_token,
+                version=version
+            )
 
-                for item in wall:
-                    if isinstance(item, dict) and len(posts) < count:
-                        posts.append(item)
+            for item in wall:
+                if isinstance(item, dict) and len(posts) < count:
+                    posts.append(item)
 
-                current_offset += 100
-                time.sleep(0.2)
-            except Exception as e:
-                print('exception: {}'.format(str(e)))
-                print('exception:(')
-                pass
+            current_offset += 100
+            time.sleep(0.2)
+        except Exception as e:
+            print('exception: {}'.format(str(e)))
+            print('exception:(')
+            pass
 
     return posts
