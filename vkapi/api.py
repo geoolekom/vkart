185a5dd07e0fd168807d15f98dc56e3c6499e092
Mogbymo <mogbymo@gmail.com>
2018-11-09 19:41:59 +0300

    Add get_api

--- vkapi/api.py
+++ vkapi/api.py
@@ -1,11 +1,15 @@
 import vk
 
+def get_api(access_token):
+    session = vk.Session(access_token=access_token)
+    api = vk.API(session, v='5.87')
+
 def group_name_from_url(url):
     if url.endswith('/'):
         url = url[:-1]
     return url.split('/')[-1]
 
-def get_goods(url):
+def get_goods(api, url):
     owner_name = group_name_from_url(url)
     owner_id = api.groups.getById(group_id=owner_name)[0]['id']
     return api.market.get(owner_id=-owner_id)
