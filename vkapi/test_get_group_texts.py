adb97e06d8cea98cea830c3a3a560ae539cbfc83
Mogbymo <mogbymo@gmail.com>
2018-11-10 13:03:28 +0300

    Add get_group_url

--- vkapi/test_get_group_texts.py
+++ vkapi/test_get_group_texts.py
@@ -1,8 +1,16 @@
-from .parameters import access_token
-from .api import *
+try:
+  from .parameters import access_token
+except:
+  from parameters import access_token
+
+try:
+  from .api import *
+except:
+  from api import *
 from pprint import pprint
 
 api = get_api()
-texts = get_group_texts(api, 'fr01ik', 10)
+#texts = get_group_texts(api, 'jumoreski', 10)
+print(get_group_url(api, 136765630))
 
-pprint(texts)
+#pprint(texts)
