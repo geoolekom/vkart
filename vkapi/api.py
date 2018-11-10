6b3286e469fb5aa96947ce0d75beb42f2f31dd79
Mogbymo <mogbymo@gmail.com>
2018-11-10 12:48:19 +0300

    Load posts without attachments

--- vkapi/api.py
+++ vkapi/api.py
@@ -80,7 +80,6 @@ def get_group_albums(api, url):
 def get_group_texts(api, url, max_posts=1e6):
     group_name = group_name_from_url(url)
     group_info = api.groups.getById(group_id=group_name, fields='status,description')[0]
-    id = group_info['id']
 
     posts = get_group_posts(api, url, max_posts)['posts']
 
@@ -192,31 +191,35 @@ class Photo(object):
 
 
 def create_post(wall):
-    if 'attachments' not in wall:
-        return False, None
-    attachment = wall['attachments'][0]
-    if 'photo' not in attachment:
-        return False, None
+    best_size = {
+        'url': None,
+        'height': None,
+        'width': None
+    }
 
-    photo = attachment['photo']
-    best_size = photo['sizes'][0]
-    best_type = best_size['type']
-    for size in photo['sizes']:
-        if vk_size_priorities[size['type']] < vk_size_priorities[best_type]:
-            best_size = size
-            best_type = size['type']
+    if 'attachments' in wall:
+        attachment = wall['attachments'][0]
+        if 'photo' in attachment:
+            photo = attachment['photo']
+            best_size = photo['sizes'][0]
+            best_type = best_size['type']
+            for size in photo['sizes']:
+                if vk_size_priorities[size['type']] < vk_size_priorities[best_type]:
+                    best_size = size
+                    best_type = size['type']
 
     return True, {
+        'pic_url': best_size['url'],
+        'height': best_size['height'],
+        'width': best_size['width'],
+
         'id': wall['id'],
         'like_count': wall['likes']['count'],
         'timestamp': wall['date'],
         'post_url': 'https://vk.com/wall'.format(wall["from_id"], wall["id"]),
         'text': wall['text'],
-        'pic_url': best_size['url'],
-        'height': best_size['height'],
-        'width': best_size['width'],
+        'group_id': - wall['from_id'],
         'rating': 0.01,
-        'group_id': - wall['from_id']
     }
 
 
@@ -253,5 +256,3 @@ def get_best_pictures(api, group_id):
     processed_posts = process_posts(posts)
     set_ratings(processed_posts)
     return [post for post in processed_posts if post['rating'] > 0.95]
-
-
